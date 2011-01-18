cdf43709b49c6ba8b5b376ac554388e20ec38579
Ross Paffett <ross@rosspaffett.com>
2011-01-18 11:17:36 -0500

    Add integration test for NICK and USER on connection establishment

--- test/integration/connection_test.rb
+++ test/integration/connection_test.rb
@@ -2,10 +2,18 @@ require "test_helper"
 
 module Tropoli
   class ConnectionTest < IntegrationTest
+    test :"nick and user commands sent to establish connection" do
+      options = { :nick => "Tropoli", :user => "tropoli", :real => "Tropoli Bot" }
+      connection(options).tap do |c|
+        assert_sent_from c, "NICK #{options[:nick]}\r\n"
+        assert_sent_from c, "USER #{options[:user]} 0 0 :#{options[:real]}\r\n"
+      end
+    end
+    
     test :"pass sent before nick and user if supplied" do
       pass = "s3cr3t"
       connection(:pass => pass).tap do |c|
-        assert_sent_from c, "PASS #{pass}"
+        assert_sent_from c, "PASS #{pass}\r\n"
       end
     end
     
