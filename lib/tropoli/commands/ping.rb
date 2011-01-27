cdc799ce14c2ee91e935d7d197b3bace75eb1e87
Ross Paffett <ross@rosspaffett.com>
2011-01-27 14:53:53 -0500

    Respond to CTCP PING requests

--- lib/tropoli/commands/ping.rb
+++ lib/tropoli/commands/ping.rb
@@ -4,7 +4,12 @@ module Tropoli
       extend Concerns::Callbacks
       
       on :ping do |message, connection|
-        connection.send_message :pong, *message.params
+        next unless message.incoming?
+        if message.ctcp?
+          connection.send_ctcp_message :pong, *message.params, :target => message.source, :type => :notice
+        else
+          connection.send_message :pong, *message.params
+        end
       end
     end
   end
--- test/integration/ping_response_test.rb
+++ test/integration/ping_response_test.rb
@@ -5,10 +5,18 @@ module Tropoli
     test :"responding to a PING message" do
       connection.tap do |c|
         assert_sent_from c, "PONG gort.nj.us.doolanshire.net" do
-          c.receive_message :ping, "gort.nj.us.doolanshire.net"
+          c.receive_line ":gort.nj.us.doolanshire.net PING gort.nj.us.doolanshire.net\r\n"
         end
         assert_sent_from c, "PONG gort.nj.us.doolanshire.net blat.ma.es.doolanshire.net" do
-          c.receive_message :ping, "gort.nj.us.doolanshire.net", "blat.ma.es.doolanshire.net"
+          c.receive_line ":server PING gort.nj.us.doolanshire.net blat.ma.es.doolanshire.net\r\n"
+        end
+      end
+    end
+    
+    test :"responding to a CTCP PING request" do
+      connection.tap do |c|
+        assert_sent_from c, "NOTICE Raws :\x01PONG signature\x01\r\n" do
+          c.receive_line ":Raws!ross@test PRIVMSG Tropoli :\x01PING signature\x01\r\n"
         end
       end
     end
